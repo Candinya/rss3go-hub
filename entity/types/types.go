90f2171c6ba6db8943691870a3c7cdc1472e8118
Candinya <dev@candinya.com>
2021-05-16 21:27:27 +0800

    add feature: omitempty

--- entity/types/types.go
+++ entity/types/types.go
@@ -31,41 +31,41 @@ type RSS3Base struct {
 	DateCreated string  `json:"date_created"`
 	DateUpdated string  `json:"date_updated"`
 	Editors     struct {
-		Blocklist []Address `json:"blocklist"`
-		Allowlist []Address `json:"allowlist"`
-	} `json:"editors"`
+		Blocklist []Address `json:"blocklist,omitempty"`
+		Allowlist []Address `json:"allowlist,omitempty"`
+	} `json:"editors,omitempty"`
 	//Items     []interface{} `json:"items"`
-	ItemsNext Address       `json:"items_next"`
+	ItemsNext Address       `json:"items_next,omitempty"`
 
 }
 
 // RSS3Persona file, Entrance
 type RSS3Persona struct {
-	RSS3Base
+	*RSS3Base
 
 	// Type extends, default to 'persona'
 	// Editors extends, default to nil
 
 	Profile struct {
-		Name   string   `json:"name"`
-		Avatar Address  `json:"avatar"`
-		Bio    string   `json:"bio"`
-		Tags   []string `json:"tags"`
+		Name   string   `json:"name,omitempty"`
+		Avatar Address  `json:"avatar,omitempty"`
+		Bio    string   `json:"bio,omitempty"`
+		Tags   []string `json:"tags,omitempty"`
 	} `json:"profile"`
 
 	Links []struct {
 		Id   Address  `json:"id"`
 		Name string   `json:"name"`
-		Tags []string `json:"tags"`
-	} `json:"links"`
+		Tags []string `json:"tags,omitempty"`
+	} `json:"links,omitempty"`
 
 	Items  []RSS3Item  `json:"items"`
-	Assets interface{} `json:"assets"`
+	Assets interface{} `json:"assets,omitempty"`
 }
 
 // RSS3Items file
 type RSS3Items struct {
-	RSS3Base
+	*RSS3Base
 
 	// Type extends, default to 'items'
 
@@ -74,7 +74,7 @@ type RSS3Items struct {
 
 // RSS3Link file
 type RSS3Link struct {
-	RSS3Base
+	*RSS3Base
 
 	// Type extends, default to 'relationship'
 
@@ -84,35 +84,35 @@ type RSS3Link struct {
 
 type RSS3OtherPersona struct {
 	Id           Address `json:"id"`
-	Verification string  `json:"verification"`
+	Verification string  `json:"verification,omitempty"`
 	Name         string  `json:"name"`
-	Avatar       Address `json:"avatar"`
-	Bio          string  `json:"bio"`
+	Avatar       Address `json:"avatar,omitempty"`
+	Bio          string  `json:"bio,omitempty"`
 }
 
 type RSS3Item struct {
 	Id            string             `json:"id"`
-	Authors       []RSS3OtherPersona `json:"authors"`
-	Title         string             `json:"title"`
-	Summary       string             `json:"summary"`
-	Tags          []string           `json:"tags"`
-	DatePublished string             `json:"date_published"`
-	DateModified  string             `json:"date_modified"`
+	Authors       []RSS3OtherPersona `json:"authors,omitempty"`
+	Title         string             `json:"title,omitempty"`
+	Summary       string             `json:"summary,omitempty"`
+	Tags          []string           `json:"tags,omitempty"`
+	DatePublished string             `json:"date_published,omitempty"`
+	DateModified  string             `json:"date_modified,omitempty"`
 
 	Contents []struct {
 		Id                Address  `json:"id"` // Link to a third party file
 		MimeType          string   `json:"mime_type"`
-		Name              string   `json:"name"`
-		Tags              []string `json:"tags"`
-		SizeInBytes       string   `json:"size_in_bytes"`
-		DurationInSeconds string   `json:"duration_in_seconds"`
-	} `json:"contents"`
+		Name              string   `json:"name,omitempty"`
+		Tags              []string `json:"tags,omitempty"`
+		SizeInBytes       string   `json:"size_in_bytes,omitempty"`
+		DurationInSeconds string   `json:"duration_in_seconds,omitempty"`
+	} `json:"contents,omitempty"`
 
 	Contexts []struct {
 		Id   Address  `json:"id"` // Link to a RSS3Items file
-		Name string   `json:"name"`
-		Tags []string `json:"tags"`
-	} `json:"contexts"`
+		Name string   `json:"name,omitempty"`
+		Tags []string `json:"tags,omitempty"`
+	} `json:"contexts,omitempty"`
 
 }
 
