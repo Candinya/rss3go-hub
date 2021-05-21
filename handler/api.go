f51b73d321cd091c78bc8e2ce0b5e96f3cef761d
Candinya <dev@candinya.com>
2021-05-21 12:39:45 +0800

    move: handlers

--- handler/api.go
+++ handler/api.go
@@ -19,7 +19,7 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
  ********************************************************************/
 
-package api
+package handler
 
 type API interface {
 
